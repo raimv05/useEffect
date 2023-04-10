6f17ec5f65c63c4638ce48bca2720bf1f62d212b
Manish Rai <manishri12345@gmail.com>
2023-04-10 14:31:06 +0530

    code uploaded

--- src/App.js
+++ src/App.js
@@ -1,25 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+//Calling useEffect everytime.
+function MyComponent() {
+  const [count, setCount] = useState(0);
 
-function App() {
+  useEffect(() => {
+    console.log("useEffect called");
+  });
+
+  function handleButtonClick() {
+    setCount(count + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={handleButtonClick}>Increment count</button>
     </div>
   );
 }
+export default MyComponent;
 
-export default App;
+//Calling useEffect only when a state of variable changes.
+
+// function MyComponent() {
+//   const [count, setCount] = useState(0);
+//   const [name, setName] = useState("John");
+
+//   useEffect(() => {
+//     console.log(`Count is ${count}`);
+//   }, [count]);
+
+//   useEffect(() => {
+//     console.log(`Name is ${name}`);
+//   }, [name]);
+
+//   const handleButtonClick = () => {
+//     setCount(count + 1);
+//   };
+
+//   const handleNameChange = (event) => {
+//     setName(event.target.value);
+//   };
+
+//   return (
+//     <div>
+//       <p>Count: {count}</p>
+//       <button onClick={handleButtonClick}>Increment</button>
+//       <p>Name: {name}</p>
+//       <input value={name} onChange={handleNameChange} />
+//     </div>
+//   );
+// }
+// export default MyComponent;
+
+//3. When a prop is passed.
+
+//Here prop is not passed.
+
+// function MyComponent({ name }) {
+//   const [greeting, setGreeting] = useState("");
+
+//   useEffect(() => {
+//     if (name) {
+//       setGreeting(`Hello, ${name}!`);
+//     }
+//   }, [name]);
+
+//   return (
+//     <div>
+//       <p>{greeting}</p>
+//     </div>
+//   );
+// }
+
+// export default MyComponent;
+
+//Here prop is passed.
+
+// function Example({ name }) {
+//   const [greeting, setGreeting] = useState("");
+
+//   useEffect(() => {
+//     console.log("useEffect called");
+//     if (name) {
+//       setGreeting(`Hello, ${name}!`);
+//     }
+//   }, [name]);
+
+//   return (
+//     <div>
+//       <p>{greeting}</p>
+//     </div>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <div>
+//       <Example name="John" />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// 4. Calling useEffect only when load for first time.
+
+// function Example() {
+//   const [count, setCount] = useState(0);
+
+//   useEffect(() => {
+//     console.log("Component loaded for the first time");
+//   }, []);
+
+//   return (
+//     <div>
+//       <p>Count: {count}</p>
+//       <button onClick={() => setCount(count + 1)}>Increment</button>
+//     </div>
+//   );
+// }
+
+// export default Example;
